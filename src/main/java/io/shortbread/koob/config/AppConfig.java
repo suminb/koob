35591d6c5812d0281c792cd152da5b9f20975ad9
Sumin Byeon <suminb@gmail.com>
2018-11-19 21:21:29 +0900

    AppConfig

---
+++ src/main/java/io/shortbread/koob/config/AppConfig.java
@@ -0,0 +1,10 @@
+package io.shortbread.koob.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"io.shortbread.koob"})
+public class AppConfig {
+}
+
--- src/test/java/io/shortbread/koob/services/ReservationServiceTests.java
+++ src/test/java/io/shortbread/koob/services/ReservationServiceTests.java
@@ -1,15 +1,22 @@
 package io.shortbread.koob.services;
 
+import io.shortbread.koob.config.AppConfig;
+import io.shortbread.koob.exceptions.InvalidRequestException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import java.time.LocalDateTime;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
 public class ReservationServiceTests {
 
-    private ReservationService reservationService = new ReservationService();
+    @Autowired
+    private ReservationService reservationService;
 
     @Test
     public void testIsValidHours() {
@@ -17,5 +24,11 @@ public class ReservationServiceTests {
         Assert.assertTrue(reservationService.isValidHours(LocalDateTime.parse("2018-11-17T13:30")));
 
         Assert.assertFalse(reservationService.isValidHours(LocalDateTime.parse("2018-11-17T00:01")));
+    }
+
+    @Test
+    public void testCreateReservation() throws InvalidRequestException {
+        reservationService.createReservation(1, "Meeting title", "Meeting description",
+                LocalDateTime.parse("2018-11-18T10:00"), LocalDateTime.parse("2018-11-18T11:00"));
     }
 }
