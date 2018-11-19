704fe08c12fbfd05251f5f1872ccfc71bba11690
Sumin Byeon <suminb@gmail.com>
2018-11-19 18:28:18 +0900

    Produce more detailed error messages

--- src/main/java/io/shortbread/koob/RestResponseEntityExceptionHandler.java
+++ src/main/java/io/shortbread/koob/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package io.shortbread.koob;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,16 +9,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     protected ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
-        String message = ex.getConstraintViolations().stream().map(v -> v.getMessage()).collect(Collectors.joining());
-        return handleExceptionInternal(ex, message, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+//        Map<String, String> messages = ex.getConstraintViolations().stream()
+//                .map(v -> Arrays.asList(v.getPropertyPath().toString(), v.getMessage()))
+//                .collect(Collectors.toMap());
+
+        JSONObject resp = new JSONObject();
+        for (ConstraintViolation violation : ex.getConstraintViolations()) {
+            resp.put(violation.getPropertyPath().toString(), violation.getMessage());
+        }
+        return handleExceptionInternal(ex, resp.toString(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
