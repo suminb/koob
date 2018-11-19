5f5f3b9ed6c78d4ed0dca5dae7b1b5ef2cef85aa
Sumin Byeon <suminb@gmail.com>
2018-11-19 19:51:32 +0900

    Render events from server data

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -8,45 +8,13 @@ import ReservationForm from './ReservationForm.js';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-const events = [
-  {
-    id: 0,
-    title: 'Board meeting',
-    start: new Date(2018, 10, 19, 9, 0, 0),
-    end: new Date(2018, 10, 19, 13, 0, 0),
-    resourceId: 1,
-  },
-  {
-    id: 1,
-    title: 'MS training',
-    allDay: false,
-    start: new Date(2018, 10, 19, 14, 0, 0),
-    end: new Date(2018, 10, 19, 16, 30, 0),
-    resourceId: 2,
-  },
-  {
-    id: 2,
-    title: 'Team lead meeting',
-    start: new Date(2018, 10, 19, 8, 30, 0),
-    end: new Date(2018, 10, 19, 12, 30, 0),
-    resourceId: 3,
-  },
-  {
-    id: 11,
-    title: 'Birthday Party',
-    start: new Date(2018, 10, 19, 7, 0, 0),
-    end: new Date(2018, 10, 19, 10, 30, 0),
-    resourceId: 3,
-  },
-]
-
 class Calendar extends Component {
     state = {
         modalFormOpen: false,
         startDatetime: null,
         endDatetime: null,
 
-        resourceMap: [],
+        rooms: [],
         reservations: [],
         reservationsDict: {}
     };
@@ -59,7 +27,7 @@ class Calendar extends Component {
             .then(resp => resp.json())
             .then(data => {
                 this.setState({
-                    resourceMap: data.map(r => {return {resourceId: r.id, resourceTitle: r.name}})
+                    rooms: data.map(r => {return {resourceId: r.id, resourceTitle: r.name}})
                 })
             });
 
@@ -71,42 +39,20 @@ class Calendar extends Component {
             .then(resp => resp.json())
             .then(data => {
                 var reservations = this.processReservations(data);
-                this.setState({
-                    reservations: reservations,
-                    reservationsDict: this.makeReservationsDict(reservations)
-                })
+                this.setState({reservations: reservations});
             });
     }
 
     processReservations(reservations) {
         return reservations.map(r => {
-            var startDatetime = new Date(Date.parse(r['start_datetime']));
-            var endDatetime = new Date(Date.parse(r['end_datetime']));
-
-            r.slot = startDatetime.getHours() * 2 + startDatetime.getMinutes() / 30;
-            r.spans = r.duration / 30;
-            return r;
+            return {
+                id: r.id,
+                resourceId: r['room_id'],
+                title: r.subject,
+                start: new Date(Date.parse(r['start_datetime'])),
+                end: new Date(Date.parse(r['end_datetime']))
+            };
         });
-    }
-
-    /**
-     * Converts a list of reservations into a dictionary (with [slot, room_id] as the key)
-     *
-     * @param {*} reservations
-     */
-    makeReservationsDict(reservations) {
-        var dict = {};
-        reservations.map(r => {
-            const key = [r.slot, r.room_id];
-
-            if (!(key in dict)) {
-                dict[key] = r;
-            }
-            else {
-                // This should not happen
-            }
-        });
-        return dict;
     }
 
     formatDatetime(date) {
@@ -123,7 +69,7 @@ class Calendar extends Component {
     }
 
     render() {
-        if (this.state.resourceMap.length == 0) {
+        if (this.state.rooms.length == 0) {
             return <div>There is no meeting room available</div>;
         }
         else {
@@ -131,11 +77,11 @@ class Calendar extends Component {
                 <BigCalendar
                     selectable
                     localizer={BigCalendar.momentLocalizer(moment)}
-                    events={events}
+                    events={this.state.reservations}
                     defaultView={BigCalendar.Views.DAY}
                     views={['day']}
                     step={30}
-                    resources={this.state.resourceMap}
+                    resources={this.state.rooms}
                     resourceIdAccessor="resourceId"
                     resourceTitleAccessor="resourceTitle"
                     startAccessor="start"
