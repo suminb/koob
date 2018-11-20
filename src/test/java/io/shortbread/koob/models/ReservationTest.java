d62168f00d57a9fb456677ae516dc510b85465d5
Sumin Byeon <suminb@gmail.com>
2018-11-21 00:32:23 +0900

    Code refactoring

--- src/test/java/io/shortbread/koob/models/ReservationTest.java
+++ src/test/java/io/shortbread/koob/models/ReservationTest.java
@@ -4,9 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import java.util.Iterator;
 
 public class ReservationTest {
     private static LocalDateTime dt(String datetime) {
@@ -22,36 +20,29 @@ public class ReservationTest {
         return reservation;
     }
 
+    private static void checkReservation(Iterator<Reservation> iter, String expectedStart, String expectedEnd) {
+        Reservation reservation = iter.next();
+
+        Assert.assertEquals(dt(expectedStart), reservation.getStart());
+        Assert.assertEquals(dt(expectedEnd), reservation.getEnd());
+    }
+
     @Test
     public void testSynthesizeAllOccurrences1() {
         Reservation reservation = makeReservation("2018-11-11T10:00", "2018-11-11T11:00", 1, 3);
 
-        List<Reservation> reservations = StreamSupport
-                .stream(reservation.synthesizeAllOccurences().spliterator(), false)
-                .collect(Collectors.toList());
-
-        Assert.assertEquals(dt("2018-11-11T10:00"), reservations.get(0).getStart());
-        Assert.assertEquals(dt("2018-11-11T11:00"), reservations.get(0).getEnd());
-
-        Assert.assertEquals(dt("2018-11-18T10:00"), reservations.get(1).getStart());
-        Assert.assertEquals(dt("2018-11-18T11:00"), reservations.get(1).getEnd());
-
-        Assert.assertEquals(dt("2018-11-25T10:00"), reservations.get(2).getStart());
-        Assert.assertEquals(dt("2018-11-25T11:00"), reservations.get(2).getEnd());
+        Iterator<Reservation> iter = reservation.synthesizeAllOccurences().iterator();
+        checkReservation(iter, "2018-11-11T10:00","2018-11-11T11:00");
+        checkReservation(iter, "2018-11-18T10:00","2018-11-18T11:00");
+        checkReservation(iter, "2018-11-25T10:00","2018-11-25T11:00");
     }
 
     @Test
     public void testSynthesizeAllOccurrences2() {
         Reservation reservation = makeReservation("2018-11-01T08:00", "2018-11-01T10:00", 2, 2);
 
-        List<Reservation> reservations = StreamSupport
-                .stream(reservation.synthesizeAllOccurences().spliterator(), false)
-                .collect(Collectors.toList());
-
-        Assert.assertEquals(dt("2018-11-01T08:00"), reservations.get(0).getStart());
-        Assert.assertEquals(dt("2018-11-01T10:00"), reservations.get(0).getEnd());
-
-        Assert.assertEquals(dt("2018-11-15T08:00"), reservations.get(1).getStart());
-        Assert.assertEquals(dt("2018-11-15T10:00"), reservations.get(1).getEnd());
+        Iterator<Reservation> iter = reservation.synthesizeAllOccurences().iterator();
+        checkReservation(iter, "2018-11-01T08:00","2018-11-01T10:00");
+        checkReservation(iter, "2018-11-15T08:00","2018-11-15T10:00");
     }
 }
