56d2660d852200ca3e0197bf0372c5e408c9d8ae
Sumin Byeon <suminb@gmail.com>
2018-11-22 09:10:35 +0900

    Remove all Java code

--- src/main/java/io/shortbread/koob/dao/RoomDAO.java
+++
@@ -1,7 +0,0 @@
-package io.shortbread.koob.dao;
-
-import io.shortbread.koob.models.Room;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoomDAO extends CrudRepository<Room, Long> {
-}
--- src/main/java/io/shortbread/koob/exceptions/InvalidReservationRequestException.java
+++
@@ -1,7 +0,0 @@
-package io.shortbread.koob.exceptions;
-
-public class InvalidReservationRequestException extends InvalidRequestException {
-    public InvalidReservationRequestException(String message) {
-        super(message);
-    }
-}
